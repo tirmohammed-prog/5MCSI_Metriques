6a341a0556fd36c49e8c725211a4982b391fa327
tirmohammed-prog <tirmohammed17@gmail.com>
2026-01-08 12:16:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
-from urllib.request import urlopen
+from urllib.request import urlopen,Request
 import sqlite3
                                                                                                                                        
 app = Flask(__name__) 
