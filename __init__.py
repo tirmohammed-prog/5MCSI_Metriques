ecd4a18fc90a5e0200e4f97051e81dfd8ab4b25d
tirmohammed-prog <tirmohammed17@gmail.com>
2026-01-08 11:38:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,5 +35,9 @@ def mongraphique():
 def monhistogramme():
     return render_template("Histogramme.html")
   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("contact.html")
+  
 if __name__ == "__main__":
   app.run(debug=True)
