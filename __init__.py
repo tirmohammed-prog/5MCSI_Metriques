21bc565ef2950d3e5ac557031cf6572a98a93356
tirmohammed-prog <tirmohammed17@gmail.com>
2026-01-08 12:04:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,9 +7,9 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__) 
 
-@app.route("/contact/")
+@app.route("/contact/") #a jour
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"   
+    return render_template("contact.html") 
   
 @app.route('/')
 def hello_world():
